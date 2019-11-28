8b4873db4b45b87930bbda54eea1e00a60321251
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-11-28 11:47:35 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/FileController.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
+public class FileController {
+
+	private final FileService fileService;
+
+	@Autowired
+	public FileController(FileService fileService) {
+		this.fileService = fileService;
+	}
+
+	@PostMapping(value = "/file/upload")
+	public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+
+		String message = "";
+
+		try {
+			fileService.storeFile(file);
+			message = "Successfully uploaded: " + file.getOriginalFilename();
+			return ResponseEntity.status(HttpStatus.OK).body(message);
+		} catch (Exception e) {
+			message = "Failed to upload: " + file.getOriginalFilename();
+			return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
+		}
+
+	}
+}
---
+++ src/main/java/com/example/demo/FileService.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileService {
+
+	private static final String FILE_DIRECTORY = "uploads";
+
+	public void storeFile(MultipartFile file) throws IOException {
+		Path filePath = Paths.get(FILE_DIRECTORY + "/" + file.getOriginalFilename());
+
+		System.out.println("File saved: " + file.getOriginalFilename());
+
+		Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+	}
+}
