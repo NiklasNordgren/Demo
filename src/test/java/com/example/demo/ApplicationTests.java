0526ec1a1896b1e571659c5d7e9f16f5847159cd
SannaLundqvist <lundqvist174@gmail.com>
2019-11-28 13:12:13 +0100

    kommentarer

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+/**
+ * https://howtodoinjava.com/spring-boot2/testing/testing-support/
+ */
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
