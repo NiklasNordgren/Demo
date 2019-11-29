26db7201951ed65b87425a682ed56b3c8a031b3c
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-11-29 12:18:45 +0100

    File Upload update

--- src/main/java/com/example/demo/FileService.java
+++ src/main/java/com/example/demo/FileService.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
@@ -15,10 +16,13 @@ public class FileService {
 	private static final String FILE_DIRECTORY = "uploads";
 
 	public void storeFile(MultipartFile file) throws IOException {
+
+		if (file.getSize() > 10485760) {
+			throw new MaxUploadSizeExceededException(file.getSize());
+		}
+
 		Path filePath = Paths.get(FILE_DIRECTORY + "/" + file.getOriginalFilename());
-
 		System.out.println("File saved: " + file.getOriginalFilename());
-
 		Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
 	}
 }
